16a6177549d8e6680ce0bdecc4832c7722f989f5
Joseph Campbell <thecanadianjoe@gmail.com>
2024-03-31 18:17:18 -0400

    added play round functionality

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
-
-
-
+let computerSelection = getComputerChoice()
+let playerSelection = prompt()
 
 
 function getComputerChoice() {
@@ -14,6 +13,43 @@ function getComputerChoice() {
     }
    
     }
+
+
+    function playRound() {
+        getComputerChoice()
+        console.log("You Selected: " + playerSelection)
+        console.log("The Computer Selected: " + computerSelection)
+        
+        if (playerSelection === "Rock" && computerSelection === "Rock") {
+            return "Tie!"
+        }   else if (playerSelection === "Rock" && computerSelection === "Paper") {
+            return "You Lose!"
+        }   else if (playerSelection === "Rock" && computerSelection === "Scissors") {
+            return "You Win!"
+        }
+        
+        if (playerSelection === "Scissors" && computerSelection === "Scissors") {
+            return "Tie!"
+        }   else if (playerSelection === "Scissors" && computerSelection === "Rock") {
+            return "You Lose!"
+        }   else if (playerSelection === "Scissors" && computerSelection === "Paper") {
+            return "You Win!"
+        }
+      
+        if (playerSelection === "Paper" && computerSelection === "Paper") {
+            return "Tie!"
+        }   else if (playerSelection === "Paper" && computerSelection === "Scissors") {
+            return "You Lose!"
+        }   else if (playerSelection === "Paper" && computerSelection === "Rock") {
+            return "You Win!"
+        }
+        
+    }
+
+function playGame() {
+    playRound()
+    console.log(playRound(playerSelection, computerSelection))
+}
 
 
 
