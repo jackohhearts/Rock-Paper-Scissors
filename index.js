b0e6485a4ad89203efcaf87470a02e98eedf09ff
Joseph Campbell <thecanadianjoe@gmail.com>
2024-03-31 20:45:08 -0400

    added playGame function to loop round five times

--- index.js
+++ index.js
@@ -18,41 +18,57 @@ function getComputerChoice() {
     function playRound() {
         let playerSelection = prompt()
         let computerSelection = getComputerChoice()
-        getComputerChoice()
+       
         console.log("You Selected: " + playerSelection)
         console.log("The Computer Selected: " + computerSelection)
         
         if (playerSelection === "Rock" && computerSelection === "Rock") {
+            console.log("Tie!")
             return "Tie!"
         }   else if (playerSelection === "Rock" && computerSelection === "Paper") {
+            console.log("You Lose!")
             return "You Lose!"
         }   else if (playerSelection === "Rock" && computerSelection === "Scissors") {
+            console.log("You Win!")
             return "You Win!"
         }
         
         if (playerSelection === "Scissors" && computerSelection === "Scissors") {
+            console.log("Tie!")
             return "Tie!"
         }   else if (playerSelection === "Scissors" && computerSelection === "Rock") {
+            console.log("You Lose!")
             return "You Lose!"
         }   else if (playerSelection === "Scissors" && computerSelection === "Paper") {
+            console.log("You Win!")
             return "You Win!"
         }
       
         if (playerSelection === "Paper" && computerSelection === "Paper") {
+            console.log("Tie!")
             return "Tie!"
         }   else if (playerSelection === "Paper" && computerSelection === "Scissors") {
+            console.log("You Lose!")
             return "You Lose!"
         }   else if (playerSelection === "Paper" && computerSelection === "Rock") {
+            console.log("You Win!")
             return "You Win!"
         }
-        
-    }
 
 console.log(playRound())
-console.log(playRound())
-console.log(playRound())
-console.log(playRound())
-console.log(playRound())
+
+        
+    }
+    function playGame() {
+    playRound()
+    playRound()
+    playRound()
+    playRound()
+    playRound()
+    }
+
+    playGame()
+
 
 
 
