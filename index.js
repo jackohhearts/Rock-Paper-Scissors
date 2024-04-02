1f5c9740e7f00b6ac8a0a181003842452093ef55
Joseph Campbell <thecanadianjoe@gmail.com>
2024-04-02 11:26:57 -0400

    added new game function and button

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 let computerScore = 0
 let playerScore = 0
+let isAlive = false
 let playerSelectionEl = document.getElementById("playerSelection-el")
 let computerSelectionEl = document.getElementById("computerSelection-el")
 let roundResultEl = document.getElementById("roundResult-el")
 let scoreEl = document.getElementById("score-el")
 let winnerEl = document.getElementById("winner-el")
+let playAgainEl = document.getElementById("playAgain-el")
+let computerSelection = ""
 
 
 
@@ -22,23 +25,27 @@ function getComputerChoice() {
 
 function playerChoiceRock() {
         playerSelection = "Rock"
+        computerSelection = getComputerChoice()
         playGame()
     }
     
 function playerChoicePaper() {
         playerSelection = "Paper"
+        computerSelection = getComputerChoice()
         playGame()
     }
 
 function playerChoiceScissors() {
         playerSelection = "Scissors"
+        computerSelection = getComputerChoice()
         playGame()
     }
 
 
     function playRound() {
+        isAlive = true
         playerSelection = playerSelection.toUpperCase()
-        let computerSelection = getComputerChoice()
+        
         computerSelection = computerSelection.toUpperCase()
         
        
@@ -102,16 +109,39 @@ function playerChoiceScissors() {
         
         scoreEl.textContent = "Your Score: " + playerScore + " " + "----- " + "Computer Score: " + computerScore
         if (playerScore === 5 && computerScore < 5) {
-           
+            rockButton.setAttribute("disabled", 1)
+            paperButton.setAttribute("disabled", 1)
+            scissorsButton.setAttribute("disabled", 1)
+           isAlive = false
             winnerEl.textContent = "You Win The Game!"
-            return "You Win The Game!"
-        } else if (playerScore < 5 && computerScore === 5) {
+            playAgainEl.innerHTML = '<button onclick="newGame()">Play Again?</button>'
             
+            return "You Win The Game!"
+
+        } else if (playerScore < 5 && computerScore === 5) {
+            rockButton.setAttribute("disabled", 1)
+            paperButton.setAttribute("disabled", 1)
+            scissorsButton.setAttribute("disabled", 1)
+            isAlive = false
             winnerEl.textContent = "You Lose The Game! The Computer Beat You!"
+            playAgainEl.innerHTML = '<button onclick="newGame()">Play Again?</button>'
             return "You Lose The Game! The Computer Beat You!"
-        }
+        } 
         
-        
+    }
+
+    function newGame() {
+        playerScore = 0
+        playerSelection = ""
+        computerSelection = ""
+        computerScore = 0
+        winnerEl.textContent = ""
+        playAgainEl.innerHTML = ""
+        roundResultEl.textContent = ""
+        rockButton.removeAttribute("disabled", 0)
+        paperButton.removeAttribute("disabled", 0)
+        scissorsButton.removeAttribute("disabled", 0)
+        playGame()
     }
 
 
